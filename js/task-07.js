4bb265ed81c980b01a6c7917426695103ff6037b
Hanna Cherednychenko <anangelann@gmail.com>
2022-01-10 05:52:34 +0200

    hw6
    hw6

---
+++ js/task-07.js
@@ -0,0 +1,10 @@
+const refs = {
+  sizeControlRef: document.querySelector('#font-size-control'),
+  textRef: document.querySelector('#text'),
+};
+
+const onInputFontResize = event => {
+  refs.textRef.style.fontSize = `${event.currentTarget.value}px`;
+};
+
+refs.sizeControlRef.addEventListener('input', onInputFontResize);
